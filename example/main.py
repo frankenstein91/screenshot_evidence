71a19a0d9026b0f73f520a88ce14f97e9b5251ed
frankenstein91 <frankenstein91.info@googlemail.com>
2018-03-20 14:16:14 +0100

    add some checks

--- example/main.py
+++ example/main.py
@@ -1,9 +1,10 @@
 import uuid
-
+import hmac, hashlib, base64
 from functions import *
 import mss
 from PIL import Image
 import xml.etree.cElementTree as ET
+version = "Test 0.1"
 
 def TakeScreenShot():
     with mss.mss() as sct:
@@ -17,7 +18,16 @@ def TakeScreenShot():
         HostsFile = GetHostsFile()
         root = ET.Element("screenshot_evidence")
         ET.SubElement(root,"HostName").text = HostName
-        ET.SubElement(root,"PicID").text = str(uuid.uuid1()).replace("-","")
+        PicID = str(uuid.uuid1()).replace("-","")
+        FileFunctionsPY = open("functions.py", "r")
+        hmacFunctionsPY = hmac.new(PicID.encode(), msg=FileFunctionsPY.read().encode(), digestmod=hashlib.sha3_512).hexdigest()
+        FileMainPY = open("main.py", "r")
+        hmacMainPY = hmac.new(PicID.encode(), msg=FileMainPY.read().encode(),digestmod=hashlib.sha3_512).hexdigest()
+        xml_Software = ET.SubElement(root, "Software", {"Name": "Python Demo SSE", "Version": version})
+        ET.SubElement(xml_Software, "File", {"Name": "functions.py"}).text = hmacFunctionsPY
+        ET.SubElement(xml_Software, "File", {"Name": "main.py"}).text = hmacMainPY
+
+        ET.SubElement(root,"PicID").text = PicID
         xml_Times = ET.SubElement(root, "ExtTimes",{"timezone": TimeZone})
         for server, EXTtime in Times.items():
             ET.SubElement(xml_Times, "Time",{"Server": server}).text = str(EXTtime)
